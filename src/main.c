621f28c9d7962e24e9203811f5243c3f48790e6b
CPunch <sethtstubbs@gmail.com>
2022-07-19 21:00:04 -0500

    Better demo

--- src/main.c
+++ src/main.c
@@ -8,20 +8,25 @@ int main()
 {
     tAbelL_layer *layer;
     ASSET_ID tilesetID;
+    int x, y;
 
     Abel_init();
 
     tilesetID = AbelA_loadAsset("res/tileset.png", ASSET_TEXTURE);
     layer = AbelL_newLayer(AbelA_getTexture(tilesetID), AbelV_newVec2(16, 16));
 
-    AbelL_addBGTile(layer, AbelV_newVec2(0, 0), 0);
-    AbelL_addBGTile(layer, AbelV_newVec2(1, 0), 1);
-    AbelL_addBGTile(layer, AbelV_newVec2(2, 0), 2);
+    /* build pretty semi-random grass field */
+    for (x = 0; x < 16; x++) {
+        for (y = 0; y < 16; y++) {
+            switch (rand() % 6) {
+            case 0: AbelL_addBGTile(layer, AbelV_newVec2(x, y), 0); break; /* grass */
+            case 1: AbelL_addBGTile(layer, AbelV_newVec2(x, y), 2); break; /* weed */
+            default:AbelL_addBGTile(layer, AbelV_newVec2(x, y), 1); break; /* empty grass */
+            }
+        }
+    }
 
-    AbelL_addBGTile(layer, AbelV_newVec2(0, 1), 0);
-    AbelL_addBGTile(layer, AbelV_newVec2(1, 1), 1);
-    AbelL_addBGTile(layer, AbelV_newVec2(2, 1), 2);
-
+    /* render to window, pause for 6 seconds and quit */
     AbelL_renderLayer(layer, NULL);
     SDL_RenderPresent(AbelR_renderer);
     SDL_Delay(6000);
